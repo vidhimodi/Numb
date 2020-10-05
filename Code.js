5f79f9ccdc25226d55d877cb5eda8f20951f2241
vidhi.m <vidhi.m@endurance.com>
2020-10-05 18:48:59 +0530

    initial commit

---
+++ Code.js
@@ -0,0 +1,43 @@
+/**
+ * Responds to a MESSAGE event in Hangouts Chat.
+ *
+ * @param {Object} event the event object from Hangouts Chat
+ */
+function onMessage(event) {
+  var message = "You just said: \"" + event.message.text + "\"";
+  return { "text": message };
+}
+
+/**
+ * Responds to an ADDED_TO_SPACE event in Hangouts Chat.
+ *
+ * @param {Object} event the event object from Hangouts Chat
+ */
+function onAddToSpace(event) {
+  var message = "";
+
+  if (event.space.singleUserBotDm) {
+    message = "Thank you for adding me to a DM, " + event.user.displayName + "!";
+  } else {
+    message = "Thank you for adding me to " +
+        (event.space.displayName ? event.space.displayName : "this chat");
+  }
+
+  if (event.message) {
+    // Bot added through @mention.
+    message = message + " and you said: \"" + event.message.text + "\"";
+  }
+
+  return { "text": message };
+}
+
+/**
+ * Responds to a REMOVED_FROM_SPACE event in Hangouts Chat.
+ *
+ * @param {Object} event the event object from Hangouts Chat
+ */
+function onRemoveFromSpace(event) {
+  console.info("Bot removed from ",
+      (event.space.name ? event.space.name : "this chat"));
+}
+
